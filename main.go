670bf6aed4e39a1554d541c22653f6e109504ac1
Mohammadreza Varasteh <varastehmr@gmail.com>
2024-07-04 01:39:13 +0330

    Add rabbitMQ example

--- main.go
+++ main.go
@@ -1,20 +1,111 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net/http"
-	"qbc/backend/deps"
-	"qbc/backend/server"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
-	log.Println("Setting Up the Server...")
-	db := deps.CreateNewDB()
-	emailServer := deps.NewEmailServer()
-	s := server.NewServer(db, emailServer)
-	log.Println("HTTP Server Running on http://localhost:8000")
-	err := http.ListenAndServe(":8000", s)
+var emails = []string{
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+	"qbc1@gmail.com",
+}
+
+func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatal("Error Starting server. Error: ", err.Error())
+		log.Panicf("%s: %s", msg, err)
 	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"email", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for _, email := range emails {
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				DeliveryMode: amqp.Persistent,
+				ContentType:  "text/plain",
+				Body:         []byte(email),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s\n", email)
+	}
+
 }
