3088522f0d516916d2fd73d45068a67df6281fe0
Mohammadreza Varasteh <varastehmr@gmail.com>
2024-07-05 10:27:29 +0330

    fixup

--- server/fib.go
+++ server/fib.go
@@ -5,12 +5,12 @@ import (
 	"strconv"
 )
 
-func calculate_fib(num int) int {
+func calculateFib(num int) int {
 	if num <= 1 {
 		return num
 	}
 
-	return calculate_fib(num-2) + calculate_fib(num-1)
+	return calculateFib(num-2) + calculateFib(num-1)
 }
 
 func (s *server) HandleFib() http.HandlerFunc {
@@ -19,13 +19,13 @@ func (s *server) HandleFib() http.HandlerFunc {
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
 		num, err := strconv.Atoi(r.PathValue("num"))
-		if err != nil || num < 0 {
+		if err != nil || num <= 0 {
 			s.respond(w, r, ErrorResponse{Error: "Invalid Number"}, http.StatusBadRequest)
 			return
 		}
-		answer := calculate_fib(num)
+		answer := calculateFib(num)
 
-		w.Header().Set("Cache-Control", "max-age=31536000") // Cache the result for a year
+		w.Header().Set("Cache-Control", "public, max-age=31536000") // Cache the result for a year
 		s.respond(w, r, response{Answer: answer}, http.StatusOK)
 	}
 }
