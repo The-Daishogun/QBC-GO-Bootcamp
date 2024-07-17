04cffa1531d0b3e889829e2b9c655c7ad189d44a
Mohammadreza Varasteh <varastehmr@gmail.com>
2024-07-18 02:29:23 +0330

    add nginx

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"qbc/backend/deps"
 	"qbc/backend/server"
 )
 
+var port int
+
+func init() {
+	flag.IntVar(&port, "port", 8000, "The port to run the server on")
+	flag.Parse()
+}
+
 func main() {
 	log.Println("Setting Up the Server...")
 	db, _ := deps.CreateNewDB("db.sqlite")
 	emailServer := deps.NewEmailServer()
 	s := server.NewServer(db, emailServer)
-	log.Println("HTTP Server Running on http://localhost:8000")
-	err := http.ListenAndServe(":8000", s)
+	log.Printf("HTTP Server Running on http://localhost:%d\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), s)
 	if err != nil {
 		log.Fatal("Error Starting server. Error: ", err.Error())
 	}
